7efd2578d88bf369027dd1c0a95b382e41d73431
Restyled.io <commits@restyled.io>
2023-04-27 17:21:57 +0000

    Restyled by clang-format

--- generator.h
+++ generator.h
@@ -3,20 +3,19 @@
 #include "stdio.h"
 #include <stdlib.h>
 #include <string.h>
-//static char * code_op [] = { "LDC", "LDV", "STORE", "APPEL", "ENTREE", "SORTIE", "RETOUR", "ADD", "MUL", "DIV",
-//                          "SUB", "INF", "INFE", "SUP", "SUPE", "DIF", "EGAL"};
+// static char * code_op [] = { "LDC", "LDV", "STORE", "APPEL", "ENTREE",
+// "SORTIE", "RETOUR", "ADD", "MUL", "DIV",
+//                           "SUB", "INF", "INFE", "SUP", "SUPE", "DIF",
+//                           "EGAL"};
 
-typedef struct ENTREE_CODE
-{
-    char* code_op;
-    int operande;
-    char* nomFct;
+typedef struct ENTREE_CODE {
+  char *code_op;
+  int operande;
+  char *nomFct;
 } ENTREE_CODE;
 
-
-
-ENTREE_CODE creerEnt (const char* code, int op, const char* nomfonc);
-ENTREE_CODE creerOp (const char* code,int op);
-ENTREE_CODE creerCode (const char* code);
+ENTREE_CODE creerEnt(const char *code, int op, const char *nomfonc);
+ENTREE_CODE creerOp(const char *code, int op);
+ENTREE_CODE creerCode(const char *code);
 
 #endif // GENERATOR_H_INCLUDED
